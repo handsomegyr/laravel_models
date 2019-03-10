15d77aa90e45c6f8b6abfce88386b24591dcdfff
handsomegyr <handsomegyr@126.com>
2019-03-10 13:24:35 +0800

    修改

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# laravel的orm要求数据库表的结构需要增加3个字段 created_at,updated_at,deleted_at 和我另外一个库 handsomegyr/model 不相符合
-# 
+# models
--- autoload.php
+++ autoload.php
@@ -1,7 +1,3 @@
 <?php
-require_once __DIR__ . '/lib/Utils/MongoId.php';
-require_once __DIR__ . '/lib/Utils/MongoDate.php';
-require_once __DIR__ . '/lib/Utils/MongoRegex.php';
-require_once __DIR__ . '/lib/Utils/functions.php';
 require __DIR__ . '/lib/App/Autoloader.php';
 App\Autoloader::register();
